0e4ca624ad8c4efd085332ef3d15d4b66d05c205
ayush-thakur <thakurayush511@gmail.com>
2020-06-02 16:31:52 +0530

    updtaed

---
+++ src/js/custom.js
@@ -0,0 +1,32 @@
+
+$('.slider-image').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<img id="nextt" src="./src./image/arrowl.png"></img>',
+    prevArrow: '<img id="prevv" src="./src./image/arrowr.png"></img>',
+
+  });
+
+
+
+
+  $('.first').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10px',
+    nextArrow: '<img id="nextt" src="./src./image/arrowdd.png"></img>',
+    prevArrow: '<img id="prevv" src="./src./image/arrowddd.png"></img>',
+
+  });
+ 
+  
